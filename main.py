d95ad06ef5e096bb732d58f2b39d11f9e3a092a2
Rodrigo <rodrihercar24@gmail.com>
2025-07-22 09:38:20 -0600

    Primera parte del menu principal

---
+++ main.py
@@ -0,0 +1,14 @@
+ventas = []
+
+while True:
+    print("Menu de analisis de ventas")
+    print("1. Ingresar lista de ventas\n"
+          "2. Mostrar todas las ventas ingresadas\n"
+          "3. Calcular la ventas mas alta y mas baja\n"
+          "4. Calcular promedio de ventas\n"
+          "5. Contar cuantos dias superaron los Q. 1000\n"
+          "6. Buscar si una venta existe\n"
+          "7. Clasificar venta"
+          "8. Salir")
+    select = input("Selecciona una opcion: ")
+    match select:
